cea3dc2c83a2082cbf31bde7c9b9a754b63a6f3d
UlnamSong <gydect48@gmail.com>
2016-11-18 21:55:06 +0900

    Initial Commit
    Good Job

---
+++ SleepApp/app/src/main/java/com/isteam/sleepapp/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.isteam.sleepapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    final Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 5s = 5000ms
+                finish();
+            }
+        }, 2000);
+    }
+}
